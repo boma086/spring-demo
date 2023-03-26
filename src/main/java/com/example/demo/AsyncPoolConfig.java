191456bfdec76d0a1823c00ed1df3c82b3c01f1f
boma086 <Firekiss@999>
2023-03-26 08:11:49 +0800

    delete

--- src/main/java/com/example/demo/AsyncPoolConfig.java
+++ src/main/java/com/example/demo/AsyncPoolConfig.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.context.request.async.TimeoutCallableProcessingInterceptor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//mabo
+
 /** * 异步线程配置类 */
 //@Configuration
 public class AsyncPoolConfig implements WebMvcConfigurer {
